a3475eade154e63365780058b62edfefbb663c88
Emo Kadiyski <e.kadiyski@gmail.com>
2014-04-18 15:00:37 +0300

    see #0 - new project

---
+++ Test2/Test2/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ekadiyski
+ * Date: 4/18/2014
+ * Time: 2:57 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Test2
+{
+	public class MvcApplication : HttpApplication
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.Ignore("{resource}.axd/{*pathInfo}");
+			
+			routes.MapRoute(
+				"Default",
+				"{controller}/{action}/{id}",
+				new {
+					controller = "Home",
+					action = "Index",
+					id = UrlParameter.Optional
+				});
+		}
+		
+		protected void Application_Start()
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}
