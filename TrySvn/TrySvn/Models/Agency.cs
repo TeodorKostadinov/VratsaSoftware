ee7e52c877631fe6ab3a116ae5451d3b0d94a11b
Emo Kadiyski <e.kadiyski@gmail.com>
2014-04-22 22:16:56 +0300

    NewHope

---
+++ TrySvn/TrySvn/Models/Agency.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ekadiyski
+ * Date: 4/22/2014
+ * Time: 9:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TrySvn.Models
+{
+	/// <summary>
+	/// Description of Agency.
+	/// </summary>
+	public class Agency
+	{
+		public int ID {get; set;}
+		public string Name {get; set;}
+		public string Country {get; set;}
+		public int PayRate {get; set;}
+	}
+}
