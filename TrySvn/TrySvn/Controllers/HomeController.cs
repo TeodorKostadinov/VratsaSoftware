4a40b8eafb83e32b6ecba66a8e46b2c8939e33db
Ich0 <iliyan.dimov@gmail.com>
2014-04-27 15:28:33 +0300

    Added TrySvn

---
+++ TrySvn/TrySvn/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ekadiyski
+ * Date: 4/22/2014
+ * Time: 9:31 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Web.Mvc;
+
+namespace TrySvn.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		
+		public ActionResult Contact()
+		{
+			return View();
+		}
+		
+		public ActionResult FirstModel()
+		{
+			return View();
+		}
+	}
+}
---
+++ TrySvn/TrySvn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrySvn.MvcApplication" Language="C#" %>
