737d3ae6b5fd46c6f1174c582618d4036143321b
Teodor Kostadinov <teodor.kk.93@gmail.com>
2014-05-08 14:19:34 +0300

    commit all the files

---
+++ MyWebApplication/MvcApplication1/Models/Projects.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Teodor
+ * Date: 27-Apr-14
+ * Time: 15:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace MvcApplication1.Models
+{
+	/// <summary>
+	/// Description of Project.
+	/// </summary>
+	public class Projects
+	{
+		public int ID {get;set;}
+		public String Firm{get;set;}
+		public String ProjectFileReference {get;set;}
+	}
+}
