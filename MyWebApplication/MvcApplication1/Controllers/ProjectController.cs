b12feb53b18c6dbeb0ac62157825958e95d7f6ae
Teodor Kostadinov <teodor.kk.93@gmail.com>
2014-05-11 20:35:21 +0300

    View change

---
+++ MyWebApplication/MvcApplication1/Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+	/// <summary>
+	/// Description of ProjectsController.
+	/// </summary>
+	public class ProjectController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		
+		public ActionResult ProjectDetails()
+		{
+			return View();
+		}
+	}
+}
